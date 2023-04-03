7f452c67190bea398d8cd8f39bec0d58f24dc902
abinasureskumar <abina30.08.2003@gmail.com>
2023-04-03 18:58:37 +0200

    added logo and sidenav

--- script/main.js
+++ script/main.js
@@ -1,30 +1,40 @@
 // scroll to top button
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () { scrollEvent() };
-window.addEventListener('mouseup', function (event) {
-    closeNav();
-})
-
+window.onscroll = function () {
+  scrollEvent();
+};
+window.addEventListener("mouseup", function (event) {
+  closeNav();
+});
 
 function scrollEvent() {
-    //Get the button
-    var mybutton = document.getElementById("up");
-    if (document.documentElement.scrollTop >= 20) {
-        if (mybutton.style.display == "none") {
-            mybutton.style.display = "block";
-            // console.log(document.documentElement.scrollTop)
-        }
-    } else {
-        mybutton.style.display = "none";
+  //Get the button
+  var mybutton = document.getElementById("up");
+  if (document.documentElement.scrollTop >= 20) {
+    if (mybutton.style.display == "none") {
+      mybutton.style.display = "block";
+      // console.log(document.documentElement.scrollTop)
     }
+  } else {
+    mybutton.style.display = "none";
+  }
 }
-
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-    window.setTimeout(function () {
-        location.href = "./index.html";
-    }, 800); //changes URL to home page
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+  window.setTimeout(function () {
+    location.href = "./index.html";
+  }, 800); //changes URL to home page
+}
+
+function openNav() {
+  document.getElementById("navSidebar").style.width = "250px";
+  document.getElementById("menu").style.marginRight = "0px";
+}
+
+function closeNav() {
+  document.getElementById("navSidebar").style.width = "0";
+  document.getElementById("menu").style.marginRight = "0";
 }

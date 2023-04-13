9a457c5d4660c036780523d9d950f639bf1634d0
abinasureskumar <abina30.08.2003@gmail.com>
2023-04-13 20:15:30 +0200

    B changed style and added scroll animation

--- script/main.js
+++ script/main.js
@@ -37,3 +37,60 @@ function closeNav() {
   document.getElementById("navSidebar").style.width = "0";
   document.getElementById("menu").style.marginRight = "0";
 }
+
+// text in B --> only appears when user scrolls down
+function checkForVisibility() {
+  var headers = document.querySelectorAll(".elements");
+  headers.forEach(function(elements) {
+    if (isElementInViewport(elements)) {
+      elements.classList.add("elementsVisible");
+    } else {
+      elements.classList.remove("elementsVisible");
+    }
+  });
+}
+
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+if (window.addEventListener) {
+  addEventListener("DOMContentLoaded", checkForVisibility, false);
+  addEventListener("load", checkForVisibility, false);
+  addEventListener("scroll", checkForVisibility, false);
+}function checkForVisibility() {
+  var headers = document.querySelectorAll(".elements");
+  headers.forEach(function(elements) {
+    if (isElementInViewport(elements)) {
+      elements.classList.add("elementsVisible");
+    } else {
+      elements.classList.remove("elementsVisible");
+    }
+  });
+}
+
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+if (window.addEventListener) {
+  addEventListener("DOMContentLoaded", checkForVisibility, false);
+  addEventListener("load", checkForVisibility, false);
+  addEventListener("scroll", checkForVisibility, false);
+}
